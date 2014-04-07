424843c103ed07fb7c4842f17f5eacd38a104be9
Yun-Yan Chi <jaiyalas@gmail.com>
2014-04-07 15:52:05 +0800

    setup for compiling and packaging with depednecies

---
+++ client/src/main/java/org/imbox/Imbox.java
@@ -0,0 +1,39 @@
+package org.imbox;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+ 
+public class Imbox {
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("FrameDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel emptyLabel = new JLabel("");
+        emptyLabel.setPreferredSize(new Dimension(175, 100));
+        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+        frame.pack();
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) throws IOException {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+
+        String string = "This is\na test";
+	File file = new File("/Users/jaiyalas/test.txt");
+        FileUtils.writeStringToFile(file, string);
+
+        new Thread(
+          () -> System.out.println("hello world")
+        ).start();
+
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run(){createAndShowGUI();}
+        });
+    }
+}
