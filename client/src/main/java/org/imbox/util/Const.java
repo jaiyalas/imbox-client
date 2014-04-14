6f91c70a32fb2241efa94b14eb181181cfb8aa4f
Yun-Yan Chi <jaiyalas@gmail.com>
2014-04-14 11:10:31 +0800

    move md5 and const into src

---
+++ client/src/main/java/org/imbox/util/Const.java
@@ -0,0 +1,6 @@
+package org.imbox.util.Const;
+
+public class Const{
+    public static final int size_KByte = 1024;
+    public static final int size_MByte = 1048576;
+}
---
+++ client/src/main/java/org/imbox/util/MD5.java
@@ -0,0 +1,24 @@
+package org.imbox.util.MD5;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MD5{
+    protected static byte[]        digest;
+    protected static StringBuffer  digest_strbuf;
+    protected static MessageDigest md5Instance;
+    static {
+	try {Read.md5Instance = MessageDigest.getInstance("MD5");}
+	catch(NoSuchAlgorithmException e)
+	  {throw new ExceptionInInitializerError(e);}}
+
+    // hashMD5 :: byte[] -> String
+    public static String hashMd5(byte[] rawData){
+	MD5.md5Instance.reset();
+	MD5.digest = md5Instance.digest(rawData);
+	MD5.digest_strbuf.delete(0, MD5.digest_strbuf.length());        
+	for(byte b : Read.digest)
+	    MD5.digest_strbuf.append(String.format("%02x", b & 0xff));
+	return MD5.digest_strbuf.toString();
+    }
+}
