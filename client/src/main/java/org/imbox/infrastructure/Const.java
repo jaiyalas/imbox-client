01b7e94c24f07f8eaf1eb1f7459169eadb703022
Yun-Yan Chi <jaiyalas@gmail.com>
2014-04-16 10:39:14 +0800

    added blocksize as a Const value

--- client/src/main/java/org/imbox/infrastructure/Const.java
+++ client/src/main/java/org/imbox/infrastructure/Const.java
@@ -3,4 +3,5 @@ package org.imbox.util.Const;
 public class Const{
     public static final int size_KByte = 1024;
     public static final int size_MByte = 1048576;
+    public static final int blocksizr  = 20*size_MByte;
 }
